7edeea27e38bd567e21e7067ceb6552e3da2273e
Cris <c.tuni.dominguez@gmail.com>
2020-06-12 11:24:47 +0200

    fixed script

--- seqClass.sh
+++ seqClass.sh
@@ -2,10 +2,18 @@
 seq=$1
 seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
 if [[ $seq =~ ^[ACGTU]+$ ]]; then #This conditional structure checks the identity of the string
-  if [[ $seq =~ T ]]; then #if it finds a T it must be DNa
-    echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then #if it fins a U it must be RNA
-    echo "The sequence is RNA"
+  if [[ $seq =~ T ]]; then #if it finds a T, it should look for U
+  	if [[ $seq =~ U ]]; then #if it has found a U, it cant be DNA or RNA
+    		echo "The sequence is not DNA nor RNA"
+    	else
+    		echo "The sequence is DNA" #if it only finds Ts, it should be DNA
+    	fi
+  elif [[ $seq =~ U ]]; then #if it fins a U, it should look for Ts
+  	if [[ $seq =~ T ]]; then #if it has found a T, it cant be DNA or RNA
+  		echo "The sequence is not DNA nor RNA"
+  	else
+  		echo "This sequence is RNA" #if it only finds Us, it should be RNA
+  	fi	
   else
     echo "The sequence can be DNA or RNA"
   fi
